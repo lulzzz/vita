ee34417377e7e94a2096af8380adb575f6d76fd4
Chris McKelt <chris@mckelt.com>
2018-08-18 07:37:16 +0800

    cleaning

--- source/Vita.Predictor.Tests/PredictorShould.cs
+++ source/Vita.Predictor.Tests/PredictorShould.cs
@@ -16,222 +16,230 @@ using Xunit;
 
 namespace Vita.Predictor.Tests
 {
-  [Collection("DataCollection")]
-  public class PredictorShould
-  {
-    public static string Data = PredictionModelWrapper.GetFilePath("data-sample.csv");
-    public static string Train = PredictionModelWrapper.GetFilePath("train.csv");
-    public static string Test = PredictionModelWrapper.GetFilePath("test.csv");
-
-    private readonly IPredict _predict;
-    private readonly ITextClassifier _textClassifier;
-    private readonly DataFixture _dataFixture;
-
-    public PredictorShould()
+    [Collection("DataCollection")]
+    public class PredictorShould
     {
-      _dataFixture = new DataFixture();
-      _dataFixture.Init();
-      _predict = new Predict();
-      _textClassifier = new TextClassifier(_dataFixture.Companies, _dataFixture.Localities, _dataFixture.Classifiers)
-      {
-        UseCache = false
-      };
-    }
+        public static string Data = PredictionModelWrapper.GetFilePath("data-sample.csv");
+        public static string Train = PredictionModelWrapper.GetFilePath("train.csv");
+        public static string Test = PredictionModelWrapper.GetFilePath("test.csv");
 
-    // [Fact(Skip = "run on demand")]
-    [Fact]
-    public async Task Train_model()
-    {
-      var modelpath = await _predict.TrainAsync(Train);
-      modelpath.Length.Should().NotBe(0);
-      File.Exists(modelpath).Should().Be(true);
-      Console.WriteLine(modelpath);
-    }
+        private readonly IPredict _predict;
+        private readonly ITextClassifier _textClassifier;
+        private readonly DataFixture _dataFixture;
 
-    [Fact]
-    public async Task Predict_test()
-    {
-      // run through the TEST file and predict each one
-      var contents = File.ReadAllText(Test);
-      var list = new List<KeyValuePair<string, string>>();
-      var data = FileUtil.Read(contents);
-
-      foreach (var item in data)
-      {
-        var request = new PredictionRequest
+        public PredictorShould()
         {
-          Description = item.Description,
-          Amount = item.Amount,
-          //Tags = item.Tags,
-          //Notes = item.Notes,
-          Bank = item.Bank
-          //AccountType = item.AccountType,
-          //TransactionUtcDate = item.TransactionUtcDate
-        };
-        var result = await new Predict().PredictAsync(request);
-        list.Add(new KeyValuePair<string, string>(item.SubCategory, result));
-      }
+            _dataFixture = new DataFixture();
+            _dataFixture.Init();
+            _predict = new Predict();
+            _textClassifier =
+                new TextClassifier(_dataFixture.Companies, _dataFixture.Localities, _dataFixture.Classifiers)
+                {
+                    UseCache = false
+                };
+        }
 
-      // whats our total correct?
-      var correct = list.Count(x => x.Key == x.Value);
-      var over = (double) list.Count;
-      var percentage = correct / over;
-      Console.WriteLine(percentage.ToString("P5"));
-
-      percentage.Should().BeGreaterThan(.9);
-    }
-
-    [Theory]
-    [InlineData("EFTPOS ALDI 27 CARRUM DOWNS AU", "ANZ", Categories.Groceries.Supermarkets)]
-    [InlineData("Kmart Cannington Aus", "ANZ", Categories.Shopping.OtherShopping)]
-    [InlineData("Bunnings Innaloo", "ANZ", Categories.Home.HomeImprovement)]
-    [InlineData("City Of Perth Park1 Perth", "ParkingTolls", Categories.Transport.ParkingTolls)]
-    [InlineData("Virgin Australia Airli Spring Hill Aus", "WESTPAC_INTERNET_BANKING", Categories.HolidayTravel.Flights)]
-    [InlineData("My Healthy Place Floreat", "WESTPAC_INTERNET_BANKING", Categories.HealthBeauty.Chemists)]
-    public async Task Predict_test_sample(string description, string bank, string subcategory)
-    {
-      var result = await _predict.PredictAsync(new PredictionRequest
-      {
-        Description = description,
-        Bank = bank
-      });
-
-      Console.WriteLine(result);
-      result.Should().Be(subcategory);
-    }
-
-    ///https://docs.microsoft.com/en-us/dotnet/api/microsoft.ml.models.classificationmetrics?view=ml-dotnet
-    [Fact]
-    public async Task Evaluate_accuracy_micro_greater_than_95_percent()
-    {
-      var metrics = await _predict.EvaluateAsync(Test);
-      Console.WriteLine();
-      Console.WriteLine("PredictionModel quality metrics evaluation");
-      Console.WriteLine("------------------------------------------");
-      Console.WriteLine($"AccuracyMacro: {metrics.AccuracyMacro}");
-      Console.WriteLine();
-      Console.WriteLine(
-        "the number of correctly predicted instances in the class, divided by the total number of instances in the class");
-      Console.WriteLine();
-      Console.WriteLine($"AccuracyMicro: {metrics.AccuracyMicro}");
-      Console.WriteLine("aggregate the contributions of all classes to compute the average metric");
-      Console.WriteLine();
-      Console.WriteLine($"LogLoss: {metrics.LogLoss}");
-      Console.WriteLine(
-        "Log Loss quantifies the accuracy of a classifier by penalising false classifications. Minimising the Log Loss is basically equivalent to maximising the accuracy of the classifier but has a twist see https://www.r-bloggers.com/making-sense-of-logarithmic-loss/");
-      Console.WriteLine();
-      Console.WriteLine();
-      Console.WriteLine("------------------------------------------");
-      Console.WriteLine($"confusion matrix: {metrics.ConfusionMatrix}");
-      Console.WriteLine(
-        "table that is often used to describe the performance of a classification model (or 'classifier') on a set of test data for which the true values are known.");
-      Console.WriteLine();
-
-      metrics.AccuracyMicro.Should().BeGreaterOrEqualTo(0.95);
-
-      /*
-       *
-       * Micro- and macro-averages (for whatever metric) will compute slightly different things,
-       * and thus their interpretation differs.
-       *
-       * A macro-average will compute the metric independently for each class and then take the average (hence treating all classes equally),
-       * whereas a micro-average will aggregate the contributions of all classes to compute the average metric.
-       *
-       * In a multi-class classification setup, micro-average is preferable if you suspect there might be class imbalance
-       * (i.e you may have many more examples of one class than of other classes).
-       */
-    }
-
-
-    [Fact]
-    public async Task PredictMany_will_run_through_transactions()
-    {
-      var predictionModel = await GetPredictionResults();
-      predictionModel.Count(x => x.Method == Contracts.PredictionMethod.MultiClassClassifier).Should()
-        .Be(predictionModel.Count());
-
-      var groupedList = from r in predictionModel.Select(x => x.PredictedValue)
-        group r by r
-        into newGroup
-        select new
+        // [Fact(Skip = "run on demand")]
+        [Fact]
+        public async Task Train_model()
         {
-          Category = newGroup.Key,
-          Total = newGroup.Count()
-        };
+            var modelpath = await _predict.TrainAsync(Train);
+            modelpath.Length.Should().NotBe(0);
+            File.Exists(modelpath).Should().Be(true);
+            Console.WriteLine(modelpath);
+        }
 
-      groupedList.Count().Should().BeGreaterThan(0);
-    }
-
-
-    [Fact(Skip = "")]
-    public async Task Predict_vs_text_classifier()
-    {
-      var predictionModel = await GetPredictionResults();
-      var textClassifiers = await GetTextClassifiers();
-
-      var totals1 = from r in predictionModel.Select(x => x.PredictedValue)
-        group r by r
-        into newGroup
-        select new
+        [Fact]
+        public async Task Predict_test()
         {
-          Category = newGroup.Key,
-          Total = newGroup.Count()
-        };
+            // run through the TEST file and predict each one
+            var contents = File.ReadAllText(Test);
+            var list = new List<KeyValuePair<string, string>>();
+            var data = FileUtil.Read(contents);
 
-      var totals2 = from r in textClassifiers.Select(x => x.Classifier?.SubCategory)
-        group r by r
-        into newGroup
-        select new
+            foreach (var item in data)
+            {
+                var request = new PredictionRequest
+                {
+                    Description = item.Description,
+                    Amount = item.Amount,
+                    //Tags = item.Tags,
+                    //Notes = item.Notes,
+                    Bank = item.Bank
+                    //AccountType = item.AccountType,
+                    //TransactionUtcDate = item.TransactionUtcDate
+                };
+                var result = await new Predict().PredictAsync(request);
+                list.Add(new KeyValuePair<string, string>(item.SubCategory, result));
+            }
+
+            // whats our total correct?
+            var correct = list.Count(x => x.Key == x.Value);
+            var over = (double) list.Count;
+            var percentage = correct / over;
+            Console.WriteLine(percentage.ToString("P5"));
+
+            percentage.Should().BeGreaterThan(.9);
+        }
+
+        [Theory]
+        [InlineData("EFTPOS ALDI 27 CARRUM DOWNS AU", "ANZ", Categories.Groceries.Supermarkets)]
+        [InlineData("Kmart Cannington Aus", "ANZ", Categories.Shopping.OtherShopping)]
+        [InlineData("Bunnings Innaloo", "ANZ", Categories.Home.HomeImprovement)]
+        [InlineData("City Of Perth Park1 Perth", "ParkingTolls", Categories.Transport.ParkingTolls)]
+        [InlineData("Virgin Australia Airli Spring Hill Aus", "WESTPAC_INTERNET_BANKING",
+            Categories.HolidayTravel.Flights)]
+        [InlineData("My Healthy Place Floreat", "WESTPAC_INTERNET_BANKING", Categories.HealthBeauty.Chemists)]
+        public async Task Predict_test_sample(string description, string bank, string subcategory)
         {
-          Category = newGroup.Key,
-          Total = newGroup.Count()
-        };
+            var result = await _predict.PredictAsync(new PredictionRequest
+            {
+                Description = description,
+                Bank = bank
+            });
 
-      var predictedSupermarkets = totals1.SingleOrDefault(x => x.Category == Categories.Groceries.Supermarkets);
-      
-      var textmatchedSupermarkets = totals2.SingleOrDefault(x => x.Category == Categories.Groceries.Supermarkets);
+            Console.WriteLine(result);
+            result.Should().Be(subcategory);
+        }
 
-      predictionModel.Where(x=>x.PredictedValue == Categories.Groceries.Supermarkets).Select(x=>x.Request.Description).Dump("Predicted supermarkets");
-      textClassifiers.Where(x=>x.Classifier?.SubCategory == Categories.Groceries.Supermarkets).Select(x=>x.SearchPhrase).Dump("Test classified supermarkets");
+        ///https://docs.microsoft.com/en-us/dotnet/api/microsoft.ml.models.classificationmetrics?view=ml-dotnet
+        [Fact]
+        public async Task Evaluate_accuracy_micro_greater_than_95_percent()
+        {
+            var metrics = await _predict.EvaluateAsync(Test);
+            Console.WriteLine();
+            Console.WriteLine("PredictionModel quality metrics evaluation");
+            Console.WriteLine("------------------------------------------");
+            Console.WriteLine($"AccuracyMacro: {metrics.AccuracyMacro}");
+            Console.WriteLine();
+            Console.WriteLine(
+                "the number of correctly predicted instances in the class, divided by the total number of instances in the class");
+            Console.WriteLine();
+            Console.WriteLine($"AccuracyMicro: {metrics.AccuracyMicro}");
+            Console.WriteLine("aggregate the contributions of all classes to compute the average metric");
+            Console.WriteLine();
+            Console.WriteLine($"LogLoss: {metrics.LogLoss}");
+            Console.WriteLine(
+                "Log Loss quantifies the accuracy of a classifier by penalising false classifications. Minimising the Log Loss is basically equivalent to maximising the accuracy of the classifier but has a twist see https://www.r-bloggers.com/making-sense-of-logarithmic-loss/");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("------------------------------------------");
+            Console.WriteLine($"confusion matrix: {metrics.ConfusionMatrix}");
+            Console.WriteLine(
+                "table that is often used to describe the performance of a classification model (or 'classifier') on a set of test data for which the true values are known.");
+            Console.WriteLine();
 
-      textmatchedSupermarkets.Total.Should().Be(predictedSupermarkets.Total);
+            metrics.AccuracyMicro.Should().BeGreaterOrEqualTo(0.95);
 
+            /*
+             *
+             * Micro- and macro-averages (for whatever metric) will compute slightly different things,
+             * and thus their interpretation differs.
+             *
+             * A macro-average will compute the metric independently for each class and then take the average (hence treating all classes equally),
+             * whereas a micro-average will aggregate the contributions of all classes to compute the average metric.
+             *
+             * In a multi-class classification setup, micro-average is preferable if you suspect there might be class imbalance
+             * (i.e you may have many more examples of one class than of other classes).
+             */
+        }
+
+
+        [Fact]
+        public async Task PredictMany_will_run_through_transactions()
+        {
+            var predictionModel = await GetPredictionResults();
+            predictionModel.Count(x => x.Method == Contracts.PredictionMethod.MultiClassClassifier).Should()
+                .Be(predictionModel.Count());
+
+            var groupedList = from r in predictionModel.Select(x => x.PredictedValue)
+                group r by r
+                into newGroup
+                select new
+                {
+                    Category = newGroup.Key,
+                    Total = newGroup.Count()
+                };
+
+            groupedList.Count().Should().BeGreaterThan(0);
+        }
+
+
+        [Fact]
+        public async Task Predict_vs_text_classifier()
+        {
+            var predictionModel = await GetPredictionResults();
+            var textClassifiers = await GetTextClassifiers();
+
+            predictionModel.Count().Should().Be(textClassifiers.Count(), "Not comparing the same inputs");
+
+            var totals1 = from r in predictionModel.Select(x => x.PredictedValue)
+                group r by r
+                into newGroup
+                select new
+                {
+                    Category = newGroup.Key,
+                    Total = newGroup.Count()
+                };
+
+            var totals2 = from r in textClassifiers.Select(x => x.Classifier?.SubCategory)
+                group r by r
+                into newGroup
+                select new
+                {
+                    Category = newGroup.Key,
+                    Total = newGroup.Count()
+                };
+
+
+            string expected = Categories.Groceries.LiquorStores;
+
+            var predicted = totals1.SingleOrDefault(x => x.Category ==expected);
+
+            var textmatched = totals2.SingleOrDefault(x => x.Category ==expected);
+
+            predictionModel.Where(x => x.PredictedValue == expected)
+                .Select(x => x.Request.Description).Dump($"Predicted {expected}");
+            textClassifiers.Where(x => x.Classifier?.SubCategory == expected)
+                .Select(x => x.SearchPhrase).Dump($"Test classified {expected}");
+
+            textmatched.Total.Should().Be(predicted.Total);
+        }
+
+        private async Task<IEnumerable<PredictionResult>> GetPredictionResults()
+        {
+            var json = BankStatementsFixture.Statement2;
+            var response = new BankStatementDownload(json).FetchAllResponse;
+            var account = response.Accounts.First();
+            var statementData = account.StatementData;
+
+            var data = statementData.Details.Select(x =>
+                new PredictionRequest {Bank = account.Institution, Description = x.Text, Amount = x.Amount});
+
+            var predictionModel = await _predict.PredictManyAsync(data.AsEnumerable());
+            return predictionModel;
+        }
+
+        private async Task<IEnumerable<TextClassificationResult>> GetTextClassifiers()
+        {
+            var json = BankStatementsFixture.Statement2;
+            var response = new BankStatementDownload(json).FetchAllResponse;
+            var account = response.Accounts.First();
+            var statementData = account.StatementData;
+
+            var list = new List<TextClassificationResult>();
+
+            foreach (var det in statementData.Details)
+            {
+                var match = await _textClassifier.Match(det.Text);
+                Console.WriteLine(match.Classifier != null
+                    ? $"{det.Text}  {match.Classifier.SubCategory}"
+                    : $"{det.Text}  NONE");
+
+                list.Add(match);
+            }
+
+            return list;
+        }
     }
-
-    private async Task<IEnumerable<PredictionResult>> GetPredictionResults()
-    {
-      var json = BankStatementsFixture.Statement2;
-      var response = new BankStatementDownload(json).FetchAllResponse;
-      var account = response.Accounts.First();
-      var statementData = account.StatementData;
-
-      var data = statementData.Details.Select(x =>
-        new PredictionRequest {Bank = account.Institution, Description = x.Text, Amount = x.Amount});
-
-      var predictionModel = await _predict.PredictManyAsync(data.AsEnumerable());
-      return predictionModel;
-    }
-
-    private async Task<IEnumerable<TextClassificationResult>> GetTextClassifiers()
-    {
-      var json = BankStatementsFixture.Statement2;
-      var response = new BankStatementDownload(json).FetchAllResponse;
-      var account = response.Accounts.First();
-      var statementData = account.StatementData;
-
-      var list = new List<TextClassificationResult>();
-
-      foreach (var det in statementData.Details)
-      {
-        var match = await _textClassifier.Match(det.Text);
-        Console.WriteLine(match.Classifier != null
-          ? $"{det.Text}  {match.Classifier.SubCategory}"
-          : $"{det.Text}  NONE");
-
-        list.Add(match);
-      }
-
-      return list;
-    }
-  }
 }
