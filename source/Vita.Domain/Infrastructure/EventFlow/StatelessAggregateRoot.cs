1e1a07223d6619220711092f4caa6cf9d8618a2a
Chris McKelt <chris@mckelt.com>
2018-09-04 08:00:27 +0800

    SAVEPOINT

---
+++ source/Vita.Domain/Infrastructure/EventFlow/StatelessAggregateRoot.cs
@@ -0,0 +1,21 @@
+﻿using EventFlow.Aggregates;
+using EventFlow.Core;
+
+namespace Vita.Domain.Infrastructure.EventFlow
+{
+    public class StatelessAggregateRoot<T, TId> : AggregateRoot<T, TId> where T : AggregateRoot<T, TId> where TId : IIdentity
+    {
+
+        public StatelessAggregateRoot(TId id) : base(id)
+        {
+        }
+
+        /// <summary>
+        /// Stateless so do nothing
+        /// </summary>
+        protected override void ApplyEvent(IAggregateEvent<T, TId> aggregateEvent)
+        {
+            Version++;
+        }
+    }
+}
