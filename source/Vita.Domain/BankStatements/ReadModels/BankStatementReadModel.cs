0f7fc052656387abb85efaa945e55329340bca93
chris mckelt <chris@mckelt.com>
2018-09-22 19:48:43 +0800

    SAVEPOINT

--- source/Vita.Domain/BankStatements/ReadModels/BankStatementReadModel.cs
+++ source/Vita.Domain/BankStatements/ReadModels/BankStatementReadModel.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Diagnostics;
 using System.Linq;
 using EventFlow.Aggregates;
 using EventFlow.ReadStores;
@@ -15,74 +14,81 @@ using Vita.Domain.Infrastructure.EventFlow;
 
 namespace Vita.Domain.BankStatements.ReadModels
 {
-    [Table("BankStatementReadModel")]
-    public class BankStatementReadModel : ReadModelBase, IReadModel,
-        IAmReadModelFor<BankStatementAggregate, BankStatementId, BankStatementPredicted2Event>,
-        IAmReadModelFor<BankStatementAggregate, BankStatementId, BankStatementTextMatched3Event>
+  [Table("BankStatementReadModel")]
+  public class BankStatementReadModel : ReadModelBase, IReadModel,
+    IAmReadModelFor<BankStatementAggregate, BankStatementId, BankStatementPredicted2Event>,
+    IAmReadModelFor<BankStatementAggregate, BankStatementId, BankStatementTextMatched3Event>
+  {
+    [SqlReadModelIdentityColumn] public string RequestId { get; set; }
+
+    public string Category { get; set; }
+    public string SubCategory { get; set; }
+    public string Description { get; set; }
+    public decimal Amount { get; set; }
+    public PredictionMethod Method { get; set; }
+    public DateTime? TransactionUtcDate { get; set; }
+
+    //public IEnumerable<PredictionResult> PredictionResults { get; set; }
+    //public IEnumerable<PredictionResult> Unmatched { get; set; }
+    //public Dictionary<Guid,TextClassificationResult> Matched { get; set; }
+
+    public void Apply(IReadModelContext context,
+      IDomainEvent<BankStatementAggregate, BankStatementId, BankStatementPredicted2Event> domainEvent)
     {
-        [SqlReadModelIdentityColumn] public string RequestId { get; set; }
+      AggregateId = domainEvent.AggregateIdentity.Value;
 
-        public string Category { get; set; }
-        public string SubCategory { get; set; }
-        public string Description { get; set; }
-        public decimal Amount { get; set; }
-        public PredictionMethod Method { get; set; }
-        public DateTime? TransactionUtcDate { get; set; }
+      var rm = domainEvent.AggregateEvent.PredictionResults.SingleOrDefault(x =>
+        x.Request.Id.ToString() == context.ReadModelId.ToVitaGuid().ToString());
+      if (rm == null)
+      {
+        Logger.Warning($"read model not found {context.ReadModelId}");
+        return;
+      }
 
-        //public IEnumerable<PredictionResult> PredictionResults { get; set; }
-        //public IEnumerable<PredictionResult> Unmatched { get; set; }
-        //public Dictionary<Guid,TextClassificationResult> Matched { get; set; }
+      var id = context.ReadModelId;
+      var subcategory = GetSubCategory(rm.PredictedValue);
 
-        public void Apply(IReadModelContext context,
-            IDomainEvent<BankStatementAggregate, BankStatementId, BankStatementPredicted2Event> domainEvent)
-        {
-            AggregateId = domainEvent.AggregateIdentity.Value;
-
-            var rm = domainEvent.AggregateEvent.PredictionResults.Single(x => x.Request.Id.ToString() == context.ReadModelId.ToVitaGuid().ToString());
-
-            var id = context.ReadModelId;
-            string subcategory = GetSubCategory(rm.PredictedValue);
-
-            RequestId = id;
-            Category = CategoryTypeConverter.FromSubcategory(subcategory).GetDescription();
-            SubCategory = GetSubCategory(subcategory);
-            Description = rm.Request.Description;
-            Amount = Convert.ToDecimal(rm.Request.Amount);
-            Method = PredictionMethod.MultiClassClassifier;
-            TransactionUtcDate = rm.Request.TransactionUtcDate;
-        }
-
-        public void Apply(IReadModelContext context,
-            IDomainEvent<BankStatementAggregate, BankStatementId, BankStatementTextMatched3Event> domainEvent)
-        {
-            AggregateId = domainEvent.AggregateIdentity.Value;
-            var rm = domainEvent.AggregateEvent.Matched.Single(x => x.Key.Request.Id.ToString() == context.ReadModelId.ToVitaGuid().ToString());
-
-            if (rm.Value.Classifier.SubCategory == SubCategories.Uncategorised)
-                Logger.Warning("SubCategory == SubCategories.Uncategorised", rm.Key.Request.Description);
-
-            string subcategory = GetSubCategory(rm.Value.Classifier?.SubCategory);
-
-            var id = context.ReadModelId;
-            RequestId = id;
-            Category = CategoryTypeConverter.FromSubcategory(subcategory).GetDescription();
-            SubCategory = subcategory;
-            Description = rm.Key.Request.Description;
-            Amount = Convert.ToDecimal(rm.Key.Request.Amount);
-            Method = PredictionMethod.KeywordMatch;
-            TransactionUtcDate = rm.Key.Request.TransactionUtcDate;
-        }
-
-
-        private string GetSubCategory(string text)
-        {
-            if (CategoryTypeConverter.IsValidSubCategory(text)) return text;
-
-            if (text == SubCategories.Uncategorised) return text;
-
-            if (text.ToLower() == "wages") return SubCategories.Income.SalaryWages;
-
-            throw new ApplicationException($"not valid subcategory: {text}");
-        }
+      RequestId = id;
+      Category = CategoryTypeConverter.FromSubcategory(subcategory).GetDescription();
+      SubCategory = GetSubCategory(subcategory);
+      Description = rm.Request.Description;
+      Amount = Convert.ToDecimal(rm.Request.Amount);
+      Method = PredictionMethod.MultiClassClassifier;
+      TransactionUtcDate = rm.Request.TransactionUtcDate;
     }
+
+    public void Apply(IReadModelContext context,
+      IDomainEvent<BankStatementAggregate, BankStatementId, BankStatementTextMatched3Event> domainEvent)
+    {
+      AggregateId = domainEvent.AggregateIdentity.Value;
+      var rm = domainEvent.AggregateEvent.Matched.Single(x =>
+        x.Key.Request.Id.ToString() == context.ReadModelId.ToVitaGuid().ToString());
+
+      if (rm.Value.Classifier.SubCategory == SubCategories.Uncategorised)
+        Logger.Warning("SubCategory == SubCategories.Uncategorised", rm.Key.Request.Description);
+
+      var subcategory = GetSubCategory(rm.Value.Classifier?.SubCategory);
+
+      var id = context.ReadModelId;
+      RequestId = id;
+      Category = CategoryTypeConverter.FromSubcategory(subcategory).GetDescription();
+      SubCategory = subcategory;
+      Description = rm.Key.Request.Description;
+      Amount = Convert.ToDecimal(rm.Key.Request.Amount);
+      Method = PredictionMethod.KeywordMatch;
+      TransactionUtcDate = rm.Key.Request.TransactionUtcDate;
+    }
+
+
+    private string GetSubCategory(string text)
+    {
+      if (CategoryTypeConverter.IsValidSubCategory(text)) return text;
+
+      if (text == SubCategories.Uncategorised) return text;
+
+      if (text.ToLower() == "wages") return SubCategories.Income.SalaryWages;
+
+      throw new ApplicationException($"not valid subcategory: {text}");
+    }
+  }
 }
